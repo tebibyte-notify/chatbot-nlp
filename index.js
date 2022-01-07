cc00780e72184f88920e6f4da371d802e2f874e6
xyperia <septianparapak@gmail.com>
2022-01-07 17:21:37 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { NlpManager } = require("node-nlp");
+console.log("Starting Chatbot ...");
+
+const manager = new NlpManager({ languages: ["id"] });
+
+manager.load();
+
+var readline = require("readline");
+var rl = readline.createInterface(process.stdin, process.stdout);
+console.log("Chatbot started!");
+rl.setPrompt("> ");
+rl.prompt();
+rl.on("line", async function (line) {
+    const response = await manager.process("id", line);
+    console.log(response.answer);
+    rl.prompt();
+}).on("close", function () {
+    process.exit(0);
+});
